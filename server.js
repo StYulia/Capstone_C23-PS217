3a52a8eb0f62d9464668bb94c45b1412e4406a44
Arfara Yema S <indonesianstudent14@gmail.com>
2023-06-08 19:22:47 +0700

    modified:   controller/Users.js modified:   server.js

--- server.js
+++ server.js
@@ -3,20 +3,19 @@ import db from "./config/database.js";
 import router from "./routes/index.js";
 import dotenv from "dotenv";
 import cors from "cors";
-import cookieParser from "cookie-parser";
+
 dotenv.config();
-const app=express();
+const app = express();
 
 try {
     await db.authenticate();
     console.log('database connected');
 } catch (error) {
-    console.log(error); 
+    console.log(error);
 }
 
-app.use(cors({credentials:true,origin:'http://localhost:5000'}));
-app.use(cookieParser());
+app.use(cors({ credentials: true, origin: 'http://localhost:5000' }));
 app.use(express.json());
 app.use(router);
 
-app.listen(5000,()=>console.log('Server running at port 5000'));
+app.listen(5000, () => console.log('Server running at port 5000'));
